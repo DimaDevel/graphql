532c92b0a7e4fe3896a167ebbc1d01ad002b8eca
DimaDevel <dima.ivanov.dev@gmail.com>
2019-12-03 00:07:17 +0200

    Refactoring for index file.

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,99 @@
+import uuidv4 from 'uuid/v4';
+
+const Mutation = {
+    createUser(parent, args, { db }, info) {
+        const emailTaken = db.users.some((user) => {
+            return user.email === args.data.email;
+        });
+
+        if (emailTaken) throw new Error('Email taken.'); 
+        
+        const user = {
+            id: uuidv4(),
+            ...args.data
+        };
+
+        db.users.push(user);
+
+        return user;
+    },
+    deleteUser(parent, args, { db }, info) { 
+        const userIndex = db.users.findIndex(user => user.id === args.id);
+
+        if (userIndex === -1) {
+            throw new Error('User not found .');
+        }
+
+        const deletedUsers = db.users.splice(userIndex, 1);
+
+        posts = db.posts.filter((post) => {
+            const match = post.author === args.id;
+
+            if (match) {
+                db.comments = db.comments.filter(comment => post.id !== comment.post);
+            }
+
+            return !match;
+        });
+
+        db.comments = db.comments.filter(comment => comment.author !== args.id);
+
+        return deletedUsers[0];
+    },
+    createPost(parent, args, { db }, info) {
+        const userExists = db.users.some((user) => user.id === args.data.author);
+
+        if (!userExists) {
+            throw new Error('User not found');
+        }
+
+        const post = {
+            id: uuidv4(),
+          ...args.data
+        };
+
+        db.posts.push(post);
+
+        return post;
+    },
+    deletePost(parent, args, { db }, info) {
+        const postIndex = db.posts.findIndex(post => post.id === args.id);
+
+        if (postIndex === -1) {
+            throw new Error('Post not found.');
+        }
+
+        const deletedPosts = db.posts.splice(postIndex, 1);
+
+        db.comments = db.comments.filter(comment => comment.post !== args.id);
+
+        return deletedPosts[0];
+    },
+    createComment(parent, args, { db }, info) {
+        if (!args.data.text) throw new Error('Can not create comment with empty fiels text');
+        const userExists = db.users.some(user => user.id === args.data.author);
+        const postExistAndPublished = db.posts.some(post => post.id === args.data.post && post.published);
+        
+        if (!userExists) throw new Error('User not found');
+        if (!postExistAndPublished) throw new Error('Post not found or not published');
+
+        const comment = {
+            id: uuidv4(),
+            ...args.data
+        }
+
+        db.comments.push(comment);
+        return comment;
+    },
+    deleteComment(parent, args, { db }, info) {
+        const commentIndex = db.comments.findIndex(comment => comment.id === args.id);
+
+        if (commentIndex === -1) {
+            throw new Error('Comment not found.');
+        }
+
+        return db.comments.splice(commentIndex, 1)[0];
+    }
+};
+
+export { Mutation as default };
