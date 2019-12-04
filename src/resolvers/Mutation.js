c75cc178cc510e8649f9153ac0c29d827dd6106a
DimaDevel <dima.ivanov.dev@gmail.com>
2019-12-04 23:39:55 +0200

    Added simple subscription.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -96,6 +96,27 @@ const Mutation = {
 
         return deletedPosts[0];
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args;
+
+        const post = db.posts.find(post => post.id === id);
+
+        if (!post) throw new Error('Post not found.');
+
+        if (typeof data.title === 'string') {
+            post.title = data.title;
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body;
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+
+        return post;
+    },
     createComment(parent, args, { db }, info) {
         if (!args.data.text) throw new Error('Can not create comment with empty fiels text');
         const userExists = db.users.some(user => user.id === args.data.author);
@@ -120,6 +141,20 @@ const Mutation = {
         }
 
         return db.comments.splice(commentIndex, 1)[0];
+    },
+    updateComment(parent, args, { db }, info) {
+        const { id, data } = args;
+        const comment = db.comments.find(comment => comment.id === id);
+
+        if (!comment) {
+            throw new Error('Comment not found.');
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
+
+        return comment;
     }
 };
 
